9df5a9091b403a3b44e909d42753ee3e3aad1086
NoobTheGoat <noob4496@Proton.me>
2023-11-18 11:21:17 +0530

    ye

--- v2/RuntimeScripts/UnionFixer.lua
+++ v2/RuntimeScripts/UnionFixer.lua
@@ -1,19 +1,28 @@
-for i,v in pairs(game.ServerStorage.Asset.MainModule:GetDescendants()) do
-    local unionsI = 0
-    local RequireHLOF = game.ServerStorage:FindFirstChild("RequireHLO")
-    if not RequireHLOF then
-        RequireHLOF = Instance.new("Folder")
-        RequireHLOF.Parent = game.ServerStorage
-        RequireHLOF.Name = "RequireHLO"
-    end
-    if v:IsA("UnionOperation") or v:IsA("MeshPart") then
-        unionsI = unionsI + 1
-        v:SetAttribute("name", v.Name)
-        v.Name = tostring(unionsI)
-        local RequireHLO = Instance.new("Script")
-        RequireHLO.Parent = v.Parent
-        RequireHLO.Name = "RequireHLO"
-        RequireHLO:SetAttribute("id", tostring(unionsI))
-        v.Parent = RequireHLOF
-    end
+local currentUnion = 1
+local SS = game:GetService("ServerStorage")
+local RequireHLOF = SS:FindFirstChild("RequireHLO")
+
+if not SS:FindFirstChild("Asset") then warn("Asset Folder/ModuleScript Missing! game.ServerStorage.Asset") return end
+if not SS.Asset:FindFirstChild("MainModule") then warn("MainModule Missing! game.ServerStorage.Asset.MainModule") return end
+
+if not RequireHLOF then
+    RequireHLOF = Instance.new("Folder")
+    RequireHLOF.Parent = game.ServerStorage
+    RequireHLOF.Name = "RequireHLO"
+end
+
+local function replaceUnion(object)
+    currentUnion = currentUnion + 1
+    object:SetAttribute("name", object.Name)
+    object.Name = tostring(currentUnion)
+    local RequireHLO = Instance.new("Script")
+    RequireHLO.Parent = object.Parent
+    RequireHLO.Name = "RequireHLO"
+    RequireHLO:SetAttribute("id", tostring(currentUnion))
+    object.Parent = RequireHLOF
+end
+
+for i,v in pairs(SS.Asset.MainModule:GetDescendants()) do
+    if not v:IsA("UnionOperation") or v:IsA("MeshPart") then continue end
+    replaceUnion(v)
 end
